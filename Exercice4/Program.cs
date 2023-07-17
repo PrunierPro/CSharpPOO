e6f7b9063fe3745244bb677e1a721bd622f76c7c
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-07-17 15:20:58 +0200

    Water Tank

--- Exercice4/Program.cs
+++ Exercice4/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WaterTank.ResetTankAmount();
+            WaterTank tank1 = new WaterTank();
+            WaterTank tank2 = new WaterTank(10, 20, 5);
+            tank1.ShowWeight();
+            tank2.ShowWeight();
+            Console.WriteLine("-----------------------------------------------------");
+            tank1.ShowFillLevel();
+            tank2.ShowFillLevel();
+            WaterTank.ShowTotalVolume();
+            Console.WriteLine("-----------------------------------------------------");
+            tank1.Fill(11);
+            tank2.Empty(11);
+            Console.WriteLine("-----------------------------------------------------");
+            tank1.ShowFillLevel();
+            tank2.ShowFillLevel();
+            WaterTank.ShowTotalVolume();
         }
     }
 }
---
+++ Exercice4/WaterTank.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice4
+{
+    internal class WaterTank
+    {
+        private static int TankAmount { get; set; } = 0;
+        public int ID { get; private set; }
+        public int BaseWeight { get; private set; }
+        public int MaxVolume { get; private set; }
+        public int FillLevel { get; private set; }
+        public static int TotalVolume { get; private set; }
+
+        public WaterTank(int baseWeight = 10, int maxVolume = 10, int initialFillLevel = 0)
+        {
+            TankAmount++;
+            ID = TankAmount;
+            BaseWeight = baseWeight;
+            MaxVolume = maxVolume;
+            FillLevel = initialFillLevel;
+        }
+
+        public void ShowWeight()
+        {
+            Console.WriteLine($"Total weight of water tank {ID} : {BaseWeight + FillLevel}");
+        }
+
+        public void ShowFillLevel()
+        {
+            Console.WriteLine($"Water amount in water tank {ID} : {FillLevel}");
+        }
+
+        public static void ShowTotalVolume()
+        {
+            Console.WriteLine($"Water amount in all water tanks : {TotalVolume}");
+        }
+
+        public void Fill(int waterLevel)
+        {
+            int newAmount = FillLevel + waterLevel;
+            int overflow;
+            if(newAmount <= MaxVolume)
+            {
+                FillLevel = newAmount;
+                overflow = 0;
+            } else
+            {
+                FillLevel = MaxVolume;
+                overflow = newAmount - MaxVolume;
+            }
+            Console.WriteLine($"Amount of water in water tank {ID} after adding {waterLevel} litres : {FillLevel}/{MaxVolume}");
+            if(overflow > 0) Console.WriteLine($"Excess water retrieved : {overflow}");
+        }
+
+        public void Empty(int amount)
+        {
+            int retrieved;
+            if(amount <= FillLevel)
+            {
+                FillLevel -= amount;
+                retrieved = amount;
+            } else
+            {
+                retrieved = FillLevel;
+                FillLevel = 0;
+            }
+            Console.WriteLine($"Amount of water in water tank {ID} after attempting to retrieve {amount} litres :  {FillLevel}/{MaxVolume}");
+            Console.WriteLine($"Amount retrieved : {retrieved}");
+        }
+
+        public static void ResetTankAmount()
+        {
+            TankAmount = 0;
+        }
+    }
+}
